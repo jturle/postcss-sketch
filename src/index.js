9fdf3a5a48380075aafaa20d596023406274d307
James Turle <james@foyerlive.com>
2017-06-13 16:36:44 -0400

    Works with original sketch

--- src/index.js
+++ src/index.js
@@ -12,19 +12,24 @@ const getSketchJSON = (file) => {
 
   // Quick Cache...
   let hash = md5(file);
-  if( cache[hash] )
+  if (cache[hash])
     return cache[hash];
 
   let sketchToolLocation = '/Applications/Sketch Beta.app/Contents/Resources/sketchtool/bin/sketchtool';
-  if (fs.existsSync(file)) {
-    if (fs.existsSync(sketchToolLocation)) {
+
+  // Suppor the non-beta version.
+  if (!fs.existsSync(sketchToolLocation))
+    sketchToolLocation = '/Applications/Sketch.app/Contents/Resources/sketchtool/bin/sketchtool';
+
+  if (fs.existsSync(sketchToolLocation)) {
+    if (fs.existsSync(file)) {
       let cmd = '"' + path.resolve(sketchToolLocation) + '" dump ' + path.resolve(file);
       return cache[hash] = JSON.parse(execSync(cmd));
     } else {
-      throw( 'Sketch Tool Not Found: ' + sketchToolLocation);
+      throw( 'Sketch File Not Found: ' + file );
     }
   } else {
-    throw( 'Sketch File Not Found: ' + file );
+    throw( 'Sketch Tool Not Found: ' + sketchToolLocation);
   }
 };
 
@@ -34,7 +39,7 @@ const getSketchJSON = (file) => {
  * @returns {string}
  */
 const convRGBA = (string) => {
-  let x = string.substr( 5, string.indexOf(')') - 5 ).split(',');
+  let x = string.substr(5, string.indexOf(')') - 5).split(',');
   x[0] *= 255;
   x[0] = Math.round(x[0]);
   x[1] *= 255;
@@ -44,33 +49,136 @@ const convRGBA = (string) => {
   return 'rgba(' + x.join(',') + ')';
 };
 
+/**
+ * Method to convert units from number in Sketch, to pixel string value...
+ * @param unit
+ * @returns {string}
+ */
+const convUnit = (unit) => {
+  return unit + 'px';
+};
+
+/**
+ * Method to convert units from percent in Sketch, to percent HTML string value...
+ * @param unit
+ * @returns {string}
+ */
+const percentUnit = (unit) => {
+  return Math.round( unit * 100 ) + '%';
+};
+
 module.exports = plugin('postcss-backwards', (opts) => {
   opts = opts || {};
   return (css, result) => {
 
     // Runs through all of the nodes (declorations) in the file
-    css.walkDecls(declaration => {
-      if (declaration.value.indexOf('sketch') !== -1) {
+    css.walkDecls(decl => {
+      if (decl.value.indexOf('sketch') !== -1) {
 
-        var parsedValue = valueParser(declaration.value);
+        var parsedValue = valueParser(decl.value);
+        let file = parsedValue.nodes[0].nodes[0].value;
+        let fileRef = path.join(path.dirname(decl.source.input.file), file);
+
+        if (parsedValue.nodes[1].value.indexOf('.sharedStyle') == 0) {
+          let sharedStyleName = parsedValue.nodes[1].value.substr(13);
+          let sketchData = getSketchJSON(path.resolve(fileRef));
+          // console.log('symbol time', sketchData.layerStyles);
+          let style = _.find(sketchData.layerStyles.objects, ['name', sharedStyleName]);
+          if (!style) {
+            console.log('Missing shared style: ' + sharedStyleName);
+          } else {
+
+            // Do the font color...
+            if (_.has(style.value, 'contextSettings.opacity') && _.get(style.value, 'contextSettings.opacity', 1) < 1) {
+              decl.cloneBefore({
+                prop: 'opacity',
+                value: _.get(style.value, 'contextSettings.opacity', 1)
+              });
+            }
+
+            // Do the background...
+            let fill = _.find(style.value.fills, ['isEnabled', 1]);
+            if (fill) {
+
+              if (fill.fillType == 0) { // Background-color
+                decl.cloneBefore({
+                  prop: 'background-color',
+                  value: _.get(fill, 'color.value')
+                });
+              }
+
+              if (fill.fillType == 1) { // Gradient
+                let gradRule;
+                switch( fill.gradient.gradientType )
+                {
+                  case 0:
+                    gradRule = 'linear-gradient(-90deg, ';
+                    break;
+                  case 1:
+                    console.log('Radial', fill );
+                    /* Rectangle:
+                    background-image: radial-gradient(26% 71%, #3023AE 17%, #C96DD8 85%);*/
+                    gradRule = 'radial-gradient(' + percentUnit(fill.gradient.from.x) + ' ' + percentUnit(fill.gradient.to.y) + ', ';
+                    break;
+                }
+                fill.gradient.stops.forEach((stop,idx) => {
+                  if( fill.gradient.gradientType == 1 )
+                  console.log(stop);
+                  if( idx > 0 )
+                    gradRule += ', ';
+                  gradRule += stop.color.value + ' ' + Math.round( stop.position * 100 ) + '%';
+                });
+                gradRule += ')';
+                decl.cloneBefore({
+                  prop: 'background-image',
+                  value: gradRule
+                });
+              }
+            }
+
+            // Do the border...
+            let border = _.find(style.value.borders, ['isEnabled', 1]);
+            if (border) {
+              decl.cloneBefore({
+                prop: 'border',
+                value: _.get(border, 'thickness') + 'px solid ' + _.get(border, 'color.value')
+              });
+            }
+
+            // Do the box shadow...
+            let shadow = _.find(style.value.shadows, ['isEnabled', 1]);
+            if (shadow) {
+              let shadowRule = convUnit(shadow.offsetX);
+              shadowRule += ' ' + convUnit(shadow.offsetY);
+              shadowRule += ' ' + convUnit(shadow.blurRadius);
+              shadowRule += ' ' + convUnit(shadow.spread);
+              shadowRule += ' ' + shadow.color.value;
+              decl.cloneBefore({
+                prop: 'box-shadow',
+                value: shadowRule
+              });
+
+            }
+
+            // Finally remove it...
+            decl.remove();
+          }
+        }
         if (parsedValue.nodes[1].value.indexOf('.textStyle') == 0) {
           let textStyleName = parsedValue.nodes[1].value.substr(11);
-
-          let file = parsedValue.nodes[0].nodes[0].value;
-          let fileRef = path.join(path.dirname(declaration.source.input.file), file);
 
           let sketchData = getSketchJSON(path.resolve(fileRef));
 
           let style = _.find(sketchData.layerTextStyles.objects, ['name', textStyleName]);
           if (!style) {
-            console.log('Missing style: ' + textStyleName);
+            console.log('Missing text style: ' + textStyleName);
           } else {
             // console.log('here', style.value.textStyle );
-            if (_.get(style.value, 'textStyle.NSFont.name', false)) {
+            if (_.get(style.value, 'textStyle.NSFont.family', false)) {
 
               // Do the font family & size...
               // TODO - line-height, style etc. px, pt, rem ?
-              declaration.value = _.get(style.value, 'textStyle.NSFont.attributes.NSFontSizeAttribute', 10) + 'px \'' + _.get(style.value, 'textStyle.NSFont.name', false) + '\'';
+              decl.value = _.get(style.value, 'textStyle.NSFont.attributes.NSFontSizeAttribute', 10) + 'px \'' + _.get(style.value, 'textStyle.NSFont.family', false) + '\'';
 
               // Do the font color...
               if (_.has(style.value, 'textStyle.NSColor.color')) {
@@ -78,7 +186,7 @@ module.exports = plugin('postcss-backwards', (opts) => {
                   prop: 'color',
                   value: convRGBA(_.get(style.value, 'textStyle.NSColor.color'))
                 });
-                declaration.parent.append(color);
+                decl.parent.append(color);
               }
             }
           }
@@ -89,13 +197,13 @@ module.exports = plugin('postcss-backwards', (opts) => {
         //console.log('File: ' + file);
         //console.log('SketchJSON',style.value );
         // console.log('Deep',parsedValue.nodes[0].nodes);
-        //console.log('this', declaration );
+        //console.log('this', decl );
       }
       // css.walkRules(rule => {
       //   console.log(rule);
       // })
-      // console.log(declaration);
-      //declaration.value = declaration.value.split('').reverse().join('');
+      // console.log(decl);
+      //decl.value = decl.value.split('').reverse().join('');
     });
 
   };
